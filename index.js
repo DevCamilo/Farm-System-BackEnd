3950526fd1cb625fc62c855323efecae8bb2ac13
Desarrollo <desarrollo@iMac-de-MELAO.local>
2018-12-21 19:18:25 -0500

    :sparkles: DB Conected :sparkles:

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 'use strict'
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const DB = require('./DB');
+
+mongoose.connect(`mongodb://${DB.user}:${DB.password}@${DB.hots}:${DB.port}/${DB.database}`, (err, con) => {
+    if(err){
+        console.log('Error en la conexión: ' + err);
+    } else {
+        console.log('Conexion DB Exitosa')
+    }
+});
+
 
 app.listen(3000, () => {
     console.log("Server Corriendo");
