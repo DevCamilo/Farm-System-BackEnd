b9aefc23dbcc692085ee0a8e3cdfc4a83858863c
Desarrollo <desarrollo@iMac-de-MELAO.local>
2018-12-28 16:40:55 -0500

    :arrows_clockwise: Add CORS :arrows_clockwise:

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 // Parametros base de datos
 const DB = require('./DB');
 // Parsear el body tipo JSON
 const bodyParser = require('body-parser');
-
 // Rutas API
 const ClientRoutes = require('./routes/clientRoute');
 const TaskRoutes = require('./routes/taskRoute');
@@ -24,6 +24,7 @@ mongoose.connect(`mongodb://${DB.user}:${DB.password}@${DB.host}:${DB.port}/${DB
 
 // Parsea el body del request en archivos JSON
 app.use(bodyParser.json());
+app.use(cors());
 
 // Lista de rutas
 app.use(ClientRoutes);
