2a25518e768884f1ccbe977433d335824158bf2e
Camilo <develop.reyes@gmail.com>
2019-02-05 16:44:40 -0600

    :traffic_light: More Validations of Token :traffic_light:

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,8 @@ function createToken(id) {
     const payload = {
         sub: id,
         iat: moment().unix(),
-        exp: moment().add(30, 'minutes').unix()
+        exp: moment().add(30, 'minutes').unix(),
+        secret: secret
     }
     return jwt.encode(payload, secret, 'HS256');
 }
@@ -20,9 +21,12 @@ function isAuth(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     var segments = token.split('.');
     if (segments.length !== 3) {
-        return res.status(200).send({ status: false, message: 'El token no es valido' })
+        return res.status(200).send({ status: false, message: 'El token no es valido' });
     }
     const payload = jwt.decode(token, secret, true, 'HS256');
+    if (payload.secret != secret){
+        return res.status(200).send({ status: false, message: 'El token no es valido' });
+    }
     if (payload.exp <= moment().unix()) {
         return res.status(200).send({ status: false, message: 'El token ha expirado' });
     }
